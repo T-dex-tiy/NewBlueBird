fb01fd36822c319896c2757ac45e6ed44f37b02b
T-Dex <dex.m.mills@gmail.com>
2018-06-03 13:19:00 -0600

    (chore) refactoring

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/api/form", (req, res) => {
-  console.log(req.body.Res);
+  console.log(req.body.Res, req.body.email);
   nodemailer.createTestAccount((err, account) => {
     const htmlEmail = "";
   });
