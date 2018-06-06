f2993cbf105fb00fb58fd3ab2f297b19df7d5f7b
T-Dex <dex.m.mills@gmail.com>
2018-06-06 17:52:03 -0600

    Remaining days corrected

--- cloudveil_heli_app/src/App.js
+++ cloudveil_heli_app/src/App.js
@@ -103,165 +103,175 @@ class App extends Component {
   }
 
   async newReservation(Res) {
-    let res = Object.keys(this.state.production.days).map(
-      key => this.state.production.days[key]
-    );
-    let resMapOne = Object.keys(res).map(key => {
-      let date = res[key].date;
-      let flyTime = res[key].reservationOne.timeSlot;
-      let mappedRes = date;
-      return mappedRes;
-    });
-    let resMapTwo = Object.keys(res)
-      .filter(key => {
+    if (this.state.remainingdays === 0) {
+      alert("Please contact Admin about adding more days!");
+    } else {
+      let res = Object.keys(this.state.production.days).map(
+        key => this.state.production.days[key]
+      );
+      let resMapOne = Object.keys(res).map(key => {
         let date = res[key].date;
-        let keys = res[key].reservationTwo;
-        return keys;
-      })
-      .map(keys => {
-        let date = res[keys].date;
-        let flyTime = res[keys].reservationTwo.timeSlot;
-        let mappedRes = date + " " + flyTime;
+        let flyTime = res[key].reservationOne.timeSlot;
+        let mappedRes = date;
         return mappedRes;
       });
-    let newResMapTwo = res.map(key => res[key]);
-
-    const addReservationAM = {
-      date: Res.day,
-      ref: `https://bluebirdheli-d1f5.firebaseio.com/staging/days/${Res.day}`,
-      reservationOne: {
-        groupUID: this.state.uid,
-        numberOfAttendees: Number(Res.numberOfAttendees),
-        operatingArea: Res.operatingArea,
-        pickupLocation: Res.pickupLocation,
-        pickupTime: Date.now(),
-        ref: `https://bluebirdheli-d1f5.firebaseio.com/staging/days/${Res.day}`,
-        timeSlot: Res.timeSlot
-      }
-    };
-    const addReservationPM = {
-      date: Res.day,
-      ref: `https://bluebirdheli-d1f5.firebaseio.com/staging/days/${Res.day}`,
-      reservationTwo: {
-        groupUID: this.state.uid,
-        numberOfAttendees: Number(Res.numberOfAttendees),
-        operatingArea: Res.operatingArea,
-        pickupLocation: Res.pickupLocation,
-        pickupTime: Date.now(),
-        ref: `https://bluebirdheli-d1f5.firebaseio.com/staging/days/${Res.day}`,
-        timeSlot: Res.timeSlot
-      }
-    };
-    //Have this working for entering date with reservationOne, Now I need to go through and figure out how to push reservationTwo
-    if (
-      resMapOne.includes(Res.day + " " + Res.timeSlot) &&
-      resMapTwo.includes(Res.day + " " + Res.timeSlot)
-    ) {
-      alert("All booked up! Please Select a different day!");
-    } else if (resMapOne.includes(Res.day + " " + Res.timeSlot)) {
-      alert(
-        "This time slot is not avaiable please select a different time on that day"
-      );
-    } else if (resMapTwo.includes(Res.day + " " + Res.timeSlot)) {
-      alert(
-        "This time slot is not avaiable please select a different time on that day"
-      );
-    } else {
-      //Checking days to imput either reservation One or Two
-      const checkFilter = Object.keys(res)
-        .map(key => res[key])
+      let resMapTwo = Object.keys(res)
         .filter(key => {
-          let varKey = [];
-          varKey = key;
-          const updatedReservationsAM = {
-            ...this.state.production.days,
-            [Res.day]: addReservationAM
-          };
-          console.log(updatedReservationsAM);
-          this.setState(prevState => ({
-            production: {
-              ...prevState.production,
-              days: updatedReservationsAM
-            }
-          }));
-          return varKey;
+          let date = res[key].date;
+          let keys = res[key].reservationTwo;
+          return keys;
+        })
+        .map(keys => {
+          let date = res[keys].date;
+          let flyTime = res[keys].reservationTwo.timeSlot;
+          let mappedRes = date + " " + flyTime;
+          return mappedRes;
         });
-      // This should be the area that getting reservationTwo pushed in.
+      let newResMapTwo = res.map(key => res[key]);
 
-      const checkRes = Object.keys(res).filter(key => {
-        let resOne = res[key];
-        if (resOne.date === Res.day) {
-          const updatedReservationsPM = {
-            ...this.state.production.days,
-            [Res.day]: addReservationPM
-          };
-          this.setState(prevState => ({
-            production: {
-              ...prevState.production,
-              days: updatedReservationsPM
-            }
-          }));
+      const addReservationAM = {
+        date: Res.day,
+        ref: `https://bluebirdheli-d1f5.firebaseio.com/staging/days/${Res.day}`,
+        reservationOne: {
+          groupUID: this.state.uid,
+          numberOfAttendees: Number(Res.numberOfAttendees),
+          operatingArea: Res.operatingArea,
+          pickupLocation: Res.pickupLocation,
+          pickupTime: Date.now(),
+          ref: `https://bluebirdheli-d1f5.firebaseio.com/staging/days/${
+            Res.day
+          }`,
+          timeSlot: Res.timeSlot
         }
-      });
-      alert(
-        "You are booked for " +
-          Res.operatingArea +
-          " skiing lodge on " +
-          Res.day
-      );
-      alert(
-        "You will be flying out of " +
-          Res.pickupLocation +
-          ". Your pick up time is in the " +
-          Res.timeSlot
-      );
-      const removeDays = Object.keys(this.state.production.users)
-        .map(key => this.state.production.users[key])
-        .map(key => {
-          if (key.uid === this.state.uid) {
-            const user = key;
-            const usedDay = key.remainingTrips - 1;
-            const updatedRemainingtrips = {
-              ...user,
-              remainingTrips: usedDay
+      };
+      const addReservationPM = {
+        date: Res.day,
+        ref: `https://bluebirdheli-d1f5.firebaseio.com/staging/days/${Res.day}`,
+        reservationTwo: {
+          groupUID: this.state.uid,
+          numberOfAttendees: Number(Res.numberOfAttendees),
+          operatingArea: Res.operatingArea,
+          pickupLocation: Res.pickupLocation,
+          pickupTime: Date.now(),
+          ref: `https://bluebirdheli-d1f5.firebaseio.com/staging/days/${
+            Res.day
+          }`,
+          timeSlot: Res.timeSlot
+        }
+      };
+      //Have this working for entering date with reservationOne, Now I need to go through and figure out how to push reservationTwo
+      if (
+        resMapOne.includes(Res.day + " " + Res.timeSlot) &&
+        resMapTwo.includes(Res.day + " " + Res.timeSlot)
+      ) {
+        alert("All booked up! Please Select a different day!");
+      } else if (resMapOne.includes(Res.day + " " + Res.timeSlot)) {
+        alert(
+          "This time slot is not avaiable please select a different time on that day"
+        );
+      } else if (resMapTwo.includes(Res.day + " " + Res.timeSlot)) {
+        alert(
+          "This time slot is not avaiable please select a different time on that day"
+        );
+      } else {
+        //Checking days to imput either reservation One or Two
+        const checkFilter = Object.keys(res)
+          .map(key => res[key])
+          .filter(key => {
+            let varKey = [];
+            varKey = key;
+            const updatedReservationsAM = {
+              ...this.state.production.days,
+              [Res.day]: addReservationAM
             };
-            const updatedUser = {
-              ...this.state.production.users,
-              [key.uid]: updatedRemainingtrips
+            console.log(updatedReservationsAM);
+            this.setState(prevState => ({
+              production: {
+                ...prevState.production,
+                days: updatedReservationsAM
+              }
+            }));
+            return varKey;
+          });
+        // This should be the area that getting reservationTwo pushed in.
+
+        const checkRes = Object.keys(res).filter(key => {
+          let resOne = res[key];
+          if (resOne.date === Res.day) {
+            const updatedReservationsPM = {
+              ...this.state.production.days,
+              [Res.day]: addReservationPM
             };
             this.setState(prevState => ({
               production: {
                 ...prevState.production,
-                users: updatedUser
+                days: updatedReservationsPM
               }
             }));
-            return key.remainingTrips;
           }
         });
-      console.log(res, Res);
-      console.log(
-        addReservationAM.reservationOne || addReservationPM.reservationTwo
-      );
-      const { user } = this.state;
-      const emailUserAndAdmin = await axios.post("api/form", {
-        user,
-        Res
-      });
+        alert(
+          "You are booked for " +
+            Res.operatingArea +
+            " skiing lodge on " +
+            Res.day
+        );
+        alert(
+          "You will be flying out of " +
+            Res.pickupLocation +
+            ". Your pick up time is in the " +
+            Res.timeSlot
+        );
+        const removeDays = Object.keys(this.state.production.users)
+          .map(key => this.state.production.users[key])
+          .map(key => {
+            if (key.uid === this.state.uid) {
+              const user = key;
+              const usedDay = key.remainingTrips - 1;
+              const updatedRemainingtrips = {
+                ...user,
+                remainingTrips: usedDay
+              };
+              const updatedUser = {
+                ...this.state.production.users,
+                [key.uid]: updatedRemainingtrips
+              };
+              this.setState(prevState => ({
+                production: {
+                  ...prevState.production,
+                  users: updatedUser
+                }
+              }));
+              return key.remainingTrips;
+            }
+          });
+        console.log(res, Res);
+        console.log(
+          addReservationAM.reservationOne || addReservationPM.reservationTwo
+        );
+        const { user } = this.state;
+        const emailUserAndAdmin = await axios.post("api/form", {
+          user,
+          Res
+        });
+      }
     }
   }
 
   render() {
-    let remainingFlyingDays;
     if (this.state.user !== null) {
       const remainingDays = { ...this.state.production.users };
       Object.keys(remainingDays)
         .map(key => this.state.production.users[key])
         .filter(key => {
           if (key.uid === this.state.uid) {
-            remainingFlyingDays = key.remainingTrips;
+            this.state.remainingdays = key.remainingTrips;
             return key.remainingTrips;
           }
         });
+      if (this.state.remianingdays === 0) {
+        alert("Please contact Admin to add more Days");
+      }
     }
     let userPage;
     if (this.state.user == null) {
@@ -305,7 +315,7 @@ class App extends Component {
             resetPassword={this.resetPassword.bind(this)}
             logOut={this.logOut.bind(this)}
             reservations={this.state.production.days}
-            remainingDays={remainingFlyingDays}
+            remainingDays={this.state.remainingdays}
           />
           <NavBar
             eventEmitter={this.eventEmitter}
